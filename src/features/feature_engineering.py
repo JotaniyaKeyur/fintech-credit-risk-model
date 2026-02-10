33639e25e116d8826ed72923e77f2c3e17799656
Keyur Jotaniya <keyurjotaniya37@gmail.com>
2026-02-10 22:55:09 +0530

    Refactor feature engineering calculations for clarity

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
 class FeatureEngineer(BaseEstimator, TransformerMixin):
-
     def __init__(self, columns):
         self.columns = columns
 
@@ -10,29 +9,17 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-
         # Convert numpy array back to DataFrame
         if not isinstance(X, pd.DataFrame):
             X = pd.DataFrame(X, columns=self.columns)
 
         X = X.copy()
 
-        X['TotalPastDue'] = (
-            X['NumberOfTime30-59DaysPastDueNotWorse'] +
-            X['NumberOfTime60-89DaysPastDueNotWorse'] +
-            X['NumberOfTimes90DaysLate']
-        )
-
+        X['TotalPastDue'] = (X['NumberOfTime30-59DaysPastDueNotWorse'] + X['NumberOfTime60-89DaysPastDueNotWorse'] + X['NumberOfTimes90DaysLate'])
         X['DebtRatioPerDependent'] = X['DebtRatio'] / (X['NumberOfDependents'] + 1)
-        X['UtilizationPerLine'] = (
-            X['RevolvingUtilizationOfUnsecuredLines'] /
-            (X['NumberOfOpenCreditLinesAndLoans'] + 1)
-        )
+        X['UtilizationPerLine'] = (X['RevolvingUtilizationOfUnsecuredLines'] / (X['NumberOfOpenCreditLinesAndLoans'] + 1))
         X['IncomeDebtRatio'] = X['MonthlyIncome'] / (X['DebtRatio'] + 1)
         X['HasDependents'] = (X['NumberOfDependents'] > 0).astype(int)
         X['HighDebtRatio'] = (X['DebtRatio'] > 1).astype(int)
-        X['HighUtilization'] = (
-            X['RevolvingUtilizationOfUnsecuredLines'] > 0.8
-        ).astype(int)
-
+        X['HighUtilization'] = (X['RevolvingUtilizationOfUnsecuredLines'] > 0.8).astype(int)
         return X
